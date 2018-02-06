7505954356ba28512dd4bba880601c1b51f78f2a
Michael <michael.stillwell@hotmail.com>
2018-02-06 14:54:55 -0600

    Finished

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+let baseURL = 'http://localhost:3005/students?class='
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+		.get(`${baseURL}${this.props.match.params.class}`)
+		.then(response => this.setState({ students: response.data }))
+		.catch(err => console.log(err));
   }
 
   render() {
+	  let { students } = this.state;
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+		{
+			students.map((x, y) => 
+				<Link
+					to={`/student/${x.id}`}
+				>
+				<h3 
+					key={y}
+					>{`${x.first_name} ${x.last_name}`}
+				</h3>
+				</Link>
+			)
+		}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+let baseURL = 'http://localhost:3005/students/'
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {},
+    }
+  }
+
+  componentDidMount() {
+    axios 
+      .get(`${baseURL}${this.props.match.params.id}`)
+      .then(response => this.setState({ studentInfo: response.data }))
+      .catch(err => console.log(err));
   }
 
   render() {
+    let { studentInfo } = this.state;
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>
+          {`${studentInfo.first_name} ${studentInfo.last_name}`}
+        </h1>
+        <h3>
+          Grade: {studentInfo.grade}
+        </h3>
+        <h3>
+          Email: {studentInfo.email}
+        </h3>
       </div>
     )
   }
